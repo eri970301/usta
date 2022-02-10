4c5c249e96c126731a90f4cd3c9574fa20739cf4
eri970301 <63607782+eri970301@users.noreply.github.com>
2022-02-09 21:26:11 -0500

    contador

---
+++ index.js
@@ -0,0 +1,123 @@
+var n = 0
+var tunPre = document.getElementById("contador1")
+var tunPreI = setInterval(function () {
+    n += 1
+    tunPre.textContent = n
+    if (n === 13) {
+        clearInterval(tunPreI)
+    }
+}, 100)
+
+var a = 0
+var tunPos = document.getElementById("contador2")
+var tunPosI = setInterval(function () {
+    a++
+    tunPos.textContent = a
+    if (a === 23) {
+        clearInterval(tunPosI)
+    }
+}, 100)
+
+var b = 0
+var diPre = document.getElementById("contador3")
+var diPreI = setInterval(function () {
+    b++
+    diPre.textContent = b
+    if (b === 12) {
+        clearInterval(diPreI)
+    }
+}, 100)
+
+var c = 0
+var diPos = document.getElementById("contador4")
+var diPosI = setInterval(function () {
+    c++
+    diPos.textContent = c
+    if (c === 4) {
+        clearInterval(diPosI)
+    }
+}, 100)
+
+var d = 0
+var bogPre = document.getElementById("contador5")
+var bogPreI = setInterval(function () {
+    d++
+    bogPre.textContent = d
+    if (d === 21) {
+        clearInterval(bogPreI)
+    }
+}, 100)
+
+var e = 0
+var bogPos = document.getElementById("contador6")
+var bogPosI = setInterval(function () {
+    e++
+    bogPos.textContent = e
+    if (e === 41) {
+        clearInterval(bogPosI)
+    }
+}, 100)
+
+var f = 0
+var bucPre = document.getElementById("contador7")
+var bucPreI = setInterval(function () {
+    f++
+    bucPre.textContent = f
+    if (f === 17) {
+        clearInterval(bucPreI)
+    }
+}, 100)
+
+var g = 0
+var bucPos = document.getElementById("contador8")
+var bucPosI = setInterval(function () {
+    g++
+    bucPos.textContent = g
+    if (g === 23) {
+        clearInterval(bucPosI)
+    }
+}, 100)
+
+var h = 0
+var vilPre = document.getElementById("contador9")
+var vilPreI = setInterval(function () {
+    h++
+    vilPre.textContent = h
+    if (h === 11) {
+        clearInterval(vilPreI)
+    }
+}, 100)
+
+var i = 0
+var vilPos = document.getElementById("contador10")
+var vilPosI = setInterval(function () {
+    i++
+    vilPos.textContent = i
+    if (i === 8) {
+        clearInterval(vilPosI)
+    }
+}, 100)
+
+var j = 0
+var medPre = document.getElementById("contador11")
+var medPreI = setInterval(function () {
+    j++
+    medPre.textContent = j
+    if (j === 3) {
+        clearInterval(medPreI)
+    }
+}, 100)
+
+var k = 0
+var medPos = document.getElementById("contador12")
+var medPosI = setInterval(function () {
+    k++
+    medPos.textContent = k
+    if (a === 8) {
+        clearInterval(medPosI)
+    }
+}, 100)
+
+
+
+
